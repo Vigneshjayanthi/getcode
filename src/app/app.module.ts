d0ff051222c656e7af1029c9f8553e30c20fa44d
Vignesh J M <74142510+Vigneshjayanthi@users.noreply.github.com>
2020-11-17 09:31:40 -0800

    project final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireDatabaseModule} from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true, firebase:{
+    apiKey: "AIzaSyAC_c-Fve1p3e2-mHT_I0X3BupYkJL_Mds",
+  authDomain: "onlineshopping-b2dc7.firebaseapp.com",
+  databaseURL: "https://onlineshopping-b2dc7.firebaseio.com",
+  projectId: "onlineshopping-b2dc7",
+  storageBucket: "onlineshopping-b2dc7.appspot.com",
+  messagingSenderId: "646344102749",
+  appId: "1:646344102749:web:2e7c96ea900c1a9b6dceb4",
+  measurementId: "G-2MQPGNSXCF"
+  }
 };
